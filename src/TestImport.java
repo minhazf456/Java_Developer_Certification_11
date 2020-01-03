39a458b94d8d2d2799a2b28b51181bd9f433d091
minhazf456 <minhazf@gmail.com>
2020-01-02 22:40:49 -0800

    java Programme

---
+++ src/TestImport.java
@@ -0,0 +1,16 @@
+// A single-type-import declaration
+import garden.vegetable.VineVegetable;
+
+public class TestImport {
+    public static void main(String[] args) {
+
+        // String class in the java.lang package, simple class name ok
+        // no import statement required
+        String s = "We are testing import statements";
+        System.out.println(s);
+
+        // We use simple name because we specified location in the import
+        // statement
+        VineVegetable.main(args);
+    }
+}
